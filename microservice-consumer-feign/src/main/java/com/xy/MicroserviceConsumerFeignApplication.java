3acd83ba2d7816baa1daf5dd52662ae446ef3515
zanhonglei <747060040@qq.com>
2018-08-28 20:24:57 +0800

    新增Feign项目

---
+++ microservice-consumer-feign/src/main/java/com/xy/MicroserviceConsumerFeignApplication.java
@@ -0,0 +1,12 @@
+package com.xy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceConsumerFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConsumerFeignApplication.class, args);
+    }
+}
