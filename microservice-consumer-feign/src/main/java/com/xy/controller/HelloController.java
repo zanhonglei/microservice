ea626a9a1b07fc7d270e28c2648a4e74c3a7780f
zan <747060040@qq.com>
2018-09-03 20:58:58 +0800

    zan feign Demo 测试通过

--- microservice-consumer-feign/src/main/java/com/xy/controller/HelloController.java
+++ microservice-consumer-feign/src/main/java/com/xy/controller/HelloController.java
@@ -1,4 +1,18 @@
 package com.xy.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    @Autowired
+    private UserFeignClient userFeignClient;
+
+    @GetMapping("/user/{value}")
+    public String toHelloService(@PathVariable("value") String value) {
+        return userFeignClient.testHello(value);
+    }
+
 }
