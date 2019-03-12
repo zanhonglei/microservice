232017a34db083093ba5b0126b696416718f3876
zan <747060040@qq.com>
2019-03-12 22:28:27 +0800

    添加 Config client 支持 调试未通过  config server 本地模式 调试未通过

--- microservice-config-center-client/src/main/java/com/xy/controller/ConfigInfoController.java
+++ microservice-config-center-client/src/main/java/com/xy/controller/ConfigInfoController.java
@@ -5,10 +5,10 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RefreshScope
+@RefreshScope
 @RestController
 public class ConfigInfoController {
-    @Value("${info}")
+    @Value("${info.app.name}")
     private String info;
 
     @RequestMapping("/configInfo")
